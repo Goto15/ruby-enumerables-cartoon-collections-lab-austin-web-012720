1ccab80dacd58ca6871c3287a6c9bc81037faa03
Benjamin Sullivan <benjamin.r.sullivan@gmail.com>
2020-01-21 03:21:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)
-  array.each_with_index {|x, (i + 1)| puts "#{i}. #{x}"}
+  array.each_with_index {|x, i| puts "#{i + 1}. #{x}"}
 end
 
 def summon_captain_planet# code an argument here
